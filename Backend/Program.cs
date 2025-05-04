cb3904d4fbe219f75b5e463eedb672a82f0849ed
Cesar Zavaleta Estrada <zavaletap2210@gmail.com>
2025-05-04 06:29:16 -0600

    Inyeccion de dependencias

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,6 +1,9 @@
 using Backend.DTOs;
 using Backend.Models;
+using Backend.Repository;
 using Backend.Services;
+using Backend.Validators;
+using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,10 +16,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Repository
+builder.Services.AddScoped<IRepository<Beer>, BeerRepository>();
+
 //Inyeccion de contexto ---> Entity Framework
 builder.Services.AddDbContext<StoreContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("StoreConnection")));
 
+//Validators
+builder.Services.AddScoped<IValidator<BeerInsertDto>, BeerInsertValidator>();
+builder.Services.AddScoped<IValidator<BeerUpdateDto>, BeerUpdateValidator>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
