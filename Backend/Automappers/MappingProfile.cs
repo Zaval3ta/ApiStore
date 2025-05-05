a52f97e0c83a218df4012838b5ac1aa0687534b0
Cesar Zavaleta Estrada <zavaletap2210@gmail.com>
2025-05-04 19:00:36 -0600

    Mappers

---
+++ Backend/Automappers/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Backend.DTOs;
+using Backend.Models;
+
+namespace Backend.Automappers
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        {
+            CreateMap<BeerInsertDto, Beer>();
+            CreateMap<Beer, BeerDto>()
+                .ForMember(dto => dto.Id,
+                           m => m.MapFrom(b => b.BeerID));
+        }
+    }
+}
